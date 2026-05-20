1cb16e9ca93504e13f126a037571842cb3d7130b
Tiago <tiago-decampos@hotmail.com>
2026-05-19 21:22:14 -0300

    feat(DataCapture): Subindo atualização no projeto DataCapture. Transformando os dados em JSON

--- FrontEnd Projects/DataCapture/script.js
+++ FrontEnd Projects/DataCapture/script.js
@@ -1,4 +1,6 @@
 const formulario = document.getElementById('form')
+const p = document.getElementById('p')
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -12,5 +14,13 @@ form.addEventListener('submit', (event) => {
         email: formData.get('email'),
     }
 
+    p.innerHTML = `Os dados enviados foram: ${dataToSave.name}, ${dataToSave.email}`
+    const convertToJson = JSON.stringify(dataToSave)
+
+    localStorage.setItem("usuario", convertToJson)
+
     console.log(dataToSave)
+    //console.log(convertToJson)
+    //console.log(typeof convertToJson)
+    console.log(localStorage)
 });
